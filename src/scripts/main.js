8dd4cdd828ee57bd160ac385288b8ed99bfb3137
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-06-02 13:00:53 +0300

    Refinements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
 function openMenu() {
   function hideElement(element) {
-    element.classList.add('hidden');
+    element.classList.add("hidden");
   }
 
   function showElement(element) {
-    element.classList.remove('hidden');
+    element.classList.remove("hidden");
   }
 
-  const menu = document.getElementById('menu');
-  const openMenuButton = document.getElementById('menu-open-button');
-  const closeMenuButton = document.getElementById('close-menu-button');
-  const hireUs = document.getElementById('hire-us');
+  const menu = document.getElementById("menu");
+  const openMenuButton = document.getElementById("menu-open-button");
+  const closeMenuButton = document.getElementById("close-menu-button");
+  const hireUs = document.getElementById("hire-us");
 
-  const menuNavChildren = document.getElementById('menu__nav').childNodes;
+  const menuNavChildren = document.getElementById("menu__nav").childNodes;
 
   const pageBody = document.body;
 
@@ -23,11 +23,11 @@ function openMenu() {
     showElement(openMenuButton);
 
     hideElement(menu);
-    pageBody.classList.remove('page__body--with_menu');
+    pageBody.classList.remove("page__body--with_menu");
   }
 
   function addEventListenerCloseMenuOnClick(element) {
-    element.addEventListener('click', () => {
+    element.addEventListener("click", () => {
       closeMenu();
     });
   }
@@ -35,12 +35,12 @@ function openMenu() {
   hideElement(closeMenuButton);
   hideElement(menu);
 
-  openMenuButton.addEventListener('click', () => {
+  openMenuButton.addEventListener("click", () => {
     hideElement(openMenuButton);
     showElement(closeMenuButton);
 
     showElement(menu);
-    pageBody.classList.add('page__body--with_menu');
+    pageBody.classList.add("page__body--with_menu");
   });
 
   addEventListenerCloseMenuOnClick(closeMenuButton);
@@ -53,34 +53,36 @@ function openMenu() {
 }
 
 function createSlider() {
-  const slidesAmount = 3
+  const slidesAmount = 3;
 
   let currentSlideIndex = 0;
 
-  const sliderSlides = document.querySelector('.slider__slides');
-  const arrowLeft = document.getElementById('arrow-left');
-  const arrowRight = document.getElementById('arrow-right');
+  const sliderSlides = document.querySelector(".slider__slides");
+  const arrowLeft = document.getElementById("arrow-left");
+  const arrowRight = document.getElementById("arrow-right");
 
   function appendSlide(prevSlideIndex, newSlideIndex) {
-    sliderSlides.children[prevSlideIndex].classList.remove('slider__slide--shown');
-    sliderSlides.children[newSlideIndex].classList.add('slider__slide--shown');
+    sliderSlides.children[prevSlideIndex].classList.remove(
+      "slider__slide--shown"
+    );
+    sliderSlides.children[newSlideIndex].classList.add("slider__slide--shown");
   }
 
   function handleRightArrowClick() {
     const prevSlideIndex = currentSlideIndex;
 
-    if (--currentSlideIndex < 0) {
-      currentSlideIndex = slidesAmount - 1;
+    if (++currentSlideIndex > slidesAmount - 1) {
+      currentSlideIndex = 0;
     }
 
     appendSlide(prevSlideIndex, currentSlideIndex);
-  };
+  }
 
   setTimeout(handleRightArrowClick, 3000);
 
   function createSliderInterval() {
     return setInterval(handleRightArrowClick, 10000);
-  };
+  }
 
   let interval = createSliderInterval();
 
@@ -90,35 +92,35 @@ function createSlider() {
     interval = createSliderInterval();
   }
 
-  arrowLeft.addEventListener('click', () => {
-    handleRightArrowClick();
-    
-    resetInterval();
-  });
-
-  arrowRight.addEventListener('click', () => {
+  arrowLeft.addEventListener("click", () => {
     const prevSlideIndex = currentSlideIndex;
 
-    if (++currentSlideIndex > slidesAmount - 1) {
-      currentSlideIndex = 0;
+    if (--currentSlideIndex < 0) {
+      currentSlideIndex = slidesAmount - 1;
     }
 
     appendSlide(prevSlideIndex, currentSlideIndex);
 
     resetInterval();
   });
+
+  arrowRight.addEventListener("click", () => {
+    handleRightArrowClick();
+
+    resetInterval();
+  });
 }
 
 function handleContactUsFormSubmit() {
-  const form = document.getElementById('contact-us-form');
+  const form = document.getElementById("contact-us-form");
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     form.reset();
   });
 }
 
-window.addEventListener('load', function() {
+window.addEventListener("load", function () {
   openMenu();
   createSlider();
   handleContactUsFormSubmit();
